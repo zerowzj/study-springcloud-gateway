fca9d794926d0b019e25e76bf5b7ec612cb0159d
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-12-14 17:36:39 +0800

    更新代码

--- src/main/java/study/springcloud/gateway/support/SpringBootCfg.java
+++ src/main/java/study/springcloud/gateway/support/SpringBootCfg.java
@@ -2,9 +2,7 @@ package study.springcloud.gateway.support;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.retry.annotation.EnableRetry;
 
-@EnableRetry
 @EnableEurekaClient
 @SpringBootApplication(scanBasePackages = "study.springcloud.gateway")
 public class SpringBootCfg {
