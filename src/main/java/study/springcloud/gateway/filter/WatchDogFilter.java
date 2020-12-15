be9e5e4149aa9d9584334cd4b8d7c0d5fbb24f38
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-12-15 13:31:49 +0800

    更新代码

--- src/main/java/study/springcloud/gateway/filter/WatchDogFilter.java
+++ src/main/java/study/springcloud/gateway/filter/WatchDogFilter.java
@@ -4,7 +4,7 @@ import com.google.common.base.Stopwatch;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
-import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
@@ -14,38 +14,23 @@ import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Component
-//@Order(0)
-public class WatchDogFilter implements GlobalFilter, Ordered {
-
-    private static final String START_TIME = "startTime";
+@Order(-1000)
+public class WatchDogFilter implements GlobalFilter {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        log.info(">>>>>> WatchDogFilter");
         ServerHttpRequest request = exchange.getRequest();
+        String path = request.getURI().getPath();
 
         Stopwatch stopwatch = Stopwatch.createStarted();
         Mono<Void> mono;
         try {
-            mono = chain.filter(exchange).then();
+            mono = chain.filter(exchange)
+                    .then(Mono.fromRunnable(() -> {
+                        log.info(">>>>>> [{}] cost time [{}] ms", path, stopwatch.elapsed(TimeUnit.MILLISECONDS));
+                    }));
         } finally {
-            log.info("cost time [{}] ms", stopwatch.elapsed(TimeUnit.MILLISECONDS));
         }
-//        exchange.getAttributes().put(START_TIME, System.currentTimeMillis());
-//        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-//            Long startTime = exchange.getAttribute(START_TIME);
-//            if (startTime != null) {
-//                Long executeTime = (System.currentTimeMillis() - startTime);
-//                log.info(">>>>>>>>>>>>"+exchange.getRequest().getURI().getRawPath() + " : " + executeTime + "ms");
-//            }
-//        }));
-
         return mono;
     }
-
-    @Override
-    public int getOrder() {
-        return Ordered.LOWEST_PRECEDENCE;
-    }
-
 }
