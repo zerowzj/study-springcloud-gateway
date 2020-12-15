70fbc65c8332916e6bbc42c5c6f781d1e2360a0a
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-12-15 16:49:01 +0800

    更新代码

---
+++ src/main/java/study/springcloud/gateway/support/limiter/UriKeyResolver.java
@@ -0,0 +1,21 @@
+package study.springcloud.gateway.support.limiter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component("uriKeyResolver")
+public class UriKeyResolver implements KeyResolver {
+
+    @Override
+    public Mono<String> resolve(ServerWebExchange exchange) {
+        ServerHttpRequest request = exchange.getRequest();
+        String path = request.getURI().getPath();
+        log.info(">>>>>>> {}", path);
+        return Mono.just(path);
+    }
+}
