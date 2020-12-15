8e199b691d0b31cf7ec1a52ea962898700dc9dba
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-12-15 10:36:52 +0800

    更新代码

--- src/main/java/study/springcloud/gateway/controller/FallbackController.java
+++ src/main/java/study/springcloud/gateway/controller/FallbackController.java
@@ -1,21 +1,23 @@
 package study.springcloud.gateway.controller;
 
+import com.google.common.collect.Maps;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @RestController
 public class FallbackController {
 
     @RequestMapping("/defaultFallback")
     public Map<String, String> defaultFallback() {
-        System.err.println("服务降级中");
-        Map<String, String> map = new HashMap<>();
-        map.put("resultCode", "fail");
-        map.put("resultMessage", "服务异常");
-        map.put("resultObj", "null");
+        log.info(">>>>>> 服务降级");
+        Map<String, String> map = Maps.newHashMap();
+        map.put("code", "fail");
+        map.put("desc", "服务异常");
+        map.put("data", "");
         return map;
     }
 }
--- src/main/java/study/springcloud/gateway/filter/WatchDogFilter.java
+++ src/main/java/study/springcloud/gateway/filter/WatchDogFilter.java
@@ -1,19 +1,51 @@
 package study.springcloud.gateway.filter;
 
-import org.springframework.cloud.gateway.filter.GatewayFilter;
+import com.google.common.base.Stopwatch;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
-import org.springframework.core.annotation.Order;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
 @Component
-@Order(0)
-public class WatchDogFilter implements GatewayFilter {
+//@Order(0)
+public class WatchDogFilter implements GlobalFilter, Ordered {
+
+    private static final String START_TIME = "startTime";
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-//        chain.filter(exchange).
-        return null;
+        log.info(">>>>>> WatchDogFilter");
+        ServerHttpRequest request = exchange.getRequest();
+
+        Stopwatch stopwatch = Stopwatch.createStarted();
+        Mono<Void> mono;
+        try {
+            mono = chain.filter(exchange).then();
+        } finally {
+            log.info("cost time [{}] ms", stopwatch.elapsed(TimeUnit.MILLISECONDS));
+        }
+//        exchange.getAttributes().put(START_TIME, System.currentTimeMillis());
+//        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+//            Long startTime = exchange.getAttribute(START_TIME);
+//            if (startTime != null) {
+//                Long executeTime = (System.currentTimeMillis() - startTime);
+//                log.info(">>>>>>>>>>>>"+exchange.getRequest().getURI().getRawPath() + " : " + executeTime + "ms");
+//            }
+//        }));
+
+        return mono;
     }
+
+    @Override
+    public int getOrder() {
+        return Ordered.LOWEST_PRECEDENCE;
+    }
+
 }
