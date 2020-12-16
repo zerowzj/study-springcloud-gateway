dcb0311c6c33cc2fed34cc605ea57768eaa7d83f
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-12-16 09:23:00 +0800

    更新代码

--- src/main/java/study/springcloud/gateway/support/limiter/HostAddrKeyResolver.java
+++ src/main/java/study/springcloud/gateway/support/limiter/HostAddrKeyResolver.java
@@ -1,7 +1,6 @@
 package study.springcloud.gateway.support.limiter;
 
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
-import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
---
+++ src/main/java/study/springcloud/gateway/support/limiter/RateLimitByCpuFilter.java
@@ -0,0 +1,16 @@
+package study.springcloud.gateway.support.limiter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class RateLimitByCpuFilter implements GatewayFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        return null;
+    }
+}
