b055da7ba4830e2ca6b1101c35e4c9befe49f8d7
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-12-18 10:09:50 +0800

    commit code

---
+++ src/main/java/study/springcloud/gateway/support/route/RouteCfg.java
@@ -0,0 +1,20 @@
+package study.springcloud.gateway.support.route;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class RouteCfg {
+
+//    @Bean
+//    public RouteLocator TT(RouteLocatorBuilder builder) {
+//        RouteLocator locator = builder.routes()
+//                .build();
+//
+//        return locator;
+//    }
+}
