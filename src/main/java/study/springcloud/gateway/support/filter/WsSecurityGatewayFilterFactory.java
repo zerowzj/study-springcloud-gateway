48448e748dcd6398e1310468fa817fe88e6c3938
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-12-22 16:17:21 +0800

    commit code

--- src/main/java/study/springcloud/gateway/support/filter/WsSecurityGatewayFilterFactory.java
+++ src/main/java/study/springcloud/gateway/support/filter/WsSecurityGatewayFilterFactory.java
@@ -30,5 +30,4 @@ public class WsSecurityGatewayFilterFactory extends AbstractNameValueGatewayFilt
             }
         };
     }
-
 }
--- src/main/java/study/springcloud/gateway/support/route/RouteCfg.java
+++ src/main/java/study/springcloud/gateway/support/route/RouteCfg.java
@@ -3,18 +3,18 @@ package study.springcloud.gateway.support.route;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Slf4j
 @Configuration
 public class RouteCfg {
 
-//    @Bean
-//    public RouteLocator TT(RouteLocatorBuilder builder) {
-//        RouteLocator locator = builder.routes()
-//                .build();
-//
-//        return locator;
-//    }
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.host("**.yuqiyu.com")
+                        .uri("http://blog.yuqiyu.com")
+                        .order(1)
+                        .id("blog"))
+                .build();
+    }
 }
